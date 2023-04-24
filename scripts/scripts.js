ba97bc08e97da3cc4c46fc6751aad93661c67252
Dan Vaccaro <dv.dev11@pm.me>
2023-04-24 17:15:52 -0400

    updated queries

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,20 @@
-console.log("Hello, world!");
+$(document).ready(function () {
+    $("#submit-button").click(function () {
+        //get values from all fields
+        user.email = $("#email").val();
+        user.local_airport = $("#local-airport").val();
+        user.dest_airport = $("#dest-airport").val();
+        user.depart_date = $("#depart-date").val();
+        user.return_date = $("#return-date").val()
+        user.max_price = $("#max-price").val();
+        console.log(user);
+        //if all fields have been filled
+        if (allFieldsFilled) {
+            console.log("submitting data to backend");
+        }
+        else console.log("Please fill in all fields.");
+    });
+});     
 
 //get value input in each field and store in a user object
 var user = {
