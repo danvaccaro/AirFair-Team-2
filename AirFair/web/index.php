71e9c01bc6b6d8cfe1ff41ef69c12a635e3009b8
KatherineWadhwani <123598972+KatherineWadhwani@users.noreply.github.com>
2023-04-28 00:18:19 -0700

    Add files via upload
    Added mail function

--- AirFair/web/index.php
+++ AirFair/web/index.php
@@ -11,13 +11,13 @@ use Monolog\Logger;
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 
-
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+
+//use PHPMailer\PHPMailer\Exception;
 
 // Passing true enables exceptions.
-$phpmailer = new PHPMailer(true);
+$phpmailer = new PHPMailer(false);
 
 
 $email = $_POST['email'];
@@ -29,13 +29,14 @@ $maxPrice = $_POST['max-price'];
 
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Methods: POST, GET');
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include_once("index.html"); 
+include_once("index.html");
+
 
 
 $dburl = parse_url(getenv('DATABASE_URL'));
@@ -52,89 +53,98 @@ $dsn = "pgsql:host=$host;dbname=$dbname;user=$user;port=$port;password=$password
 $db = new PDO($dsn);
 
 
-if($db){
-  if (!is_null($email) && !is_null($localAirport) && !is_null($destAirport) && !is_null($departDate) && !is_null($returnDate) && !is_null($maxPrice)){
+if ($db) {
+  if (!is_null($email) && !is_null($localAirport) && !is_null($destAirport) && !is_null($departDate) && !is_null($returnDate) && !is_null($maxPrice)) {
 
-  $sql = "INSERT INTO requests (email, localair, destair, arrival, return, maxprice) VALUES (?, ?, ?, ?, ?, ?)";
-  try {
-  $st = $db->prepare($sql);
-  $st->execute(array($email, $localAirport, $destAirport, $departDate, $returnDate, $maxPrice));
+    $time_input1 = strtotime($departDate);
+    $date_input1 = getDate($time_input1);
+    $time_input2 = strtotime($returnDate);
+    $date_input2 = getDate($time_input2);
 
 
-  echo '<script type="text/javascript">
-  alert("Success! Time to start packing.");
-  </script>';
 
-  }
 
-  catch (Exception $e) { 
-    echo '<script type="text/javascript">
-    alert("Please submit a valid request.");
-    </script>';
- }
+    $sql = "INSERT INTO requests (email, localair, destair, arrival, return, maxprice) VALUES (?, ?, ?, ?, ?, ?)";
+    try {
+
+      // Configure SMTP
+      $phpmailer->isSMTP();
+      $phpmailer->SMTPAuth = true;
+      $phpmailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+
+      // ENV Credentials
+      $phpmailer->Host = getenv("MAILERTOGO_SMTP_HOST", true);
+      $phpmailer->Port = intval(getenv("MAILERTOGO_SMTP_PORT", true));
+      $phpmailer->Username = getenv("MAILERTOGO_SMTP_USER", true);
+      $phpmailer->Password = getenv("MAILERTOGO_SMTP_PASSWORD", true);
+      $mailertogo_domain = getenv("MAILERTOGO_DOMAIN", true);
+
+      // Mail Headers
+      $phpmailer->setFrom("customersupport@{$mailertogo_domain}", "AirFair Team");
+      // Change to recipient email. Make sure to use a real email address in your tests to avoid hard bounces and protect your reputation as a sender.
+      $phpmailer->addAddress($email, "Katherine");
+
+      // Message
+      $phpmailer->isHTML(true);
+      $phpmailer->Subject = "AirFair Confirmation";
+      $phpmailer->Body = "<b>WELP...</b>\nLooks like we weren't able to complete all we set out to do. Call us the Theranos of air travel. \n May we instead offer you a cat meme?\n";
+      $phpmailer->AltBody = "<b>WELP...</b>\n It looks like we weren't able to complete all we set out to do. Call us the Theranos of air travel. \n May we instead offer you a cat meme?\n";
+      $phpmailer->AddEmbeddedImage('catmeme.webp', 'cat meme');
+      // Send the Email
+      $phpmailer->send();
+      //echo "Message has been sent";
+
+      $st = $db->prepare($sql);
+      $st->execute(array($email, $localAirport, $destAirport, $departDate, $returnDate, $maxPrice));
+
+
+      echo '<script type="text/javascript">
+      alert("Success! Time to start packing.");
+      </script>';
+
+    } catch (Exception $e) {
+      echo '<script type="text/javascript">
+      alert("Please submit a valid request.");
+      </script>';
+    }
   }
 }
 
-try {
-  // Configure SMTP
-  $phpmailer->isSMTP();
-  $phpmailer->SMTPAuth = true;
-  $phpmailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-
-  // ENV Credentials
-  $phpmailer->Host = getenv("MAILERTOGO_SMTP_HOST", true);
-  $phpmailer->Port = intval(getenv("MAILERTOGO_SMTP_PORT", true));
-  $phpmailer->Username = getenv("MAILERTOGO_SMTP_USER", true);
-  $phpmailer->Password = getenv("MAILERTOGO_SMTP_PASSWORD", true);
-  $mailertogo_domain = getenv("MAILERTOGO_DOMAIN", true);
 
 
-  // Mail Headers
-  $phpmailer->setFrom("mailer@{$mailertogo_domain}", "Mailer");
-  // Change to recipient email. Make sure to use a real email address in your tests to avoid hard bounces and protect your reputation as a sender.
-  $phpmailer->addAddress("noreply@{$mailertogo_domain}", "kat.wadhwani@gmail.com");
-
-
-  
-  // Message
-  $phpmailer->isHTML(true);
-  $phpmailer->Subject = "Mailer To Go Test";
-  $phpmailer->Body    = "<b>Hi</b>\nTest from Mailer To Go 😊\n";
-  $phpmailer->AltBody = "Hi!\nTest from Mailer To Go 😊\n";
-
-  // Send the Email
-  $phpmailer->send();
-  echo "Message has been sent";
-} catch (Exception $e) {
-  echo "Message could not be sent. Mailer Error: {$phpmailer->ErrorInfo}";
-}
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_REQUEST['password']) && $_REQUEST['password'] == "138237sdfoy238") {
 
 
   $db = new PDO($dsn);
   $sql = "SELECT * FROM requests";
-  if($db){
-    if (!is_null($email) && !is_null($localAirport) && !is_null($destAirport) && !is_null($departDate) && !is_null($returnDate) && !is_null($maxPrice)){
-  
+  $str = "";
+  if ($db) {
+
     $sql = "SELECT * FROM requests";
     $st = $db->prepare($sql);
-
     $st->execute();
     $result = $st->fetchAll(PDO::FETCH_ASSOC);
-  
-      foreach ($result as $row) {
-        $column1Value = $row['email'];
-        $column2Value = $row['localair'];
-        $column3Value = $row['destair'];
-        $column4Value = $row['arrival'];
-        $column5Value = $row['return'];
-        $column6Value = $row['maxprice'];
-        
-        echo $column5Value;
-      }
+
+    foreach ($result as $row) {
+      $column1Value = $row['email'];
+      $column2Value = $row['localair'];
+      $column3Value = $row['destair'];
+      $column4Value = $row['arrival'];
+      $column5Value = $row['return'];
+      $column6Value = $row['maxprice'];
+
+      $str = $str . ' %' . $column1Value . ' %' . $column2Value . ' %' . $column3Value . ' %' . $column4Value . ' %' . $column5Value . ' %' . $column6Value . ' %' . '//';
     }
   }
 
+  ob_clean();
+  exit($str);
 
-  
+}
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_REQUEST['message'])) {
+
+  $message = $_REQUEST['message'];
+
+}
 ?>
